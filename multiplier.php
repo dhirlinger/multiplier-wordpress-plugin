5167c44bf378d6b91c3277526d24cbb2fa9efdd3
Doug Hirlinger <doughirlinger@gmail.com>
2025-05-06 10:47:35 -0400

    first commit

---
+++ multiplier.php
@@ -0,0 +1,6 @@
+/*
+* Plugin Name: Multiplier
+* Description: Adds database tables and endpoints for use with the Multiplier React app and the Wordpress rest api for the purposes of saving user presets.
+* Author: Doug Hirlinger
+* Author URI: doughirlinger.com
+*/
